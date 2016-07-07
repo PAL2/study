15be07ee2700906bf5ace4b7d0141286ee7d2f51
Alex Polegoshko <pal16@tut.by>
2016-07-07 14:47:46 +0200

    change encoding

--- Homework/src/lesson2/Task1.java
+++ Homework/src/lesson2/Task1.java
@@ -8,7 +8,7 @@ public class Task1 {
 		/*
 		 * The output value of the length of the string to the console
 		 */
-		System.out.println("Длина строки = " + length);
+		System.out.println("Р”Р»РёРЅР° С‚РµРєСЃС‚Р° = " + length);
 		int n = length / 2; // Half-string division
 		String s1 = s.substring(0, n); // Calculating a first half string
 		String s2 = s.substring(n, length); // Calculating a second half string
