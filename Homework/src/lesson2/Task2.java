45ec5c6e6245264d63cf09921833baebdd08d2d5
Alex Polegoshko <pal16@tut.by>
2016-07-07 18:58:31 +0200

    add comments

--- Homework/src/lesson2/Task2.java
+++ Homework/src/lesson2/Task2.java
@@ -6,10 +6,10 @@ public class Task2 {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in); // Create an object of class Scanner
-		System.out.println("Введите целое число A"); // read integer A
-		int A = in.nextInt();
-		System.out.println("Введите целое число B"); // read integer B
-		int B = in.nextInt();
+		System.out.println("Введите целое число A");
+		int A = in.nextInt(); // read integer A
+		System.out.println("Введите целое число B");
+		int B = in.nextInt(); // read integer B
 		/*
 		 * Compare the number of A and B and display compare result
 		 */
