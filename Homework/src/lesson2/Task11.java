26b4c89eabb8e643d5a662793378582392949035
Alex Polegoshko <pal16@tut.by>
2016-07-08 14:11:33 +0200

    First commit

---
+++ Homework/src/lesson2/Task11.java
@@ -0,0 +1,14 @@
+package lesson2;
+
+public class Task11 {
+
+	public static void main(String[] args) {
+		int x =1;
+		while (x <= 10) {
+			System.out.println("x=" + x + ", x^2=" + x*x);
+			x++;
+		}
+
+	}
+
+}
