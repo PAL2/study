d351f0c3455d250f44286945c72845d4f1c5807f
Alex Polegoshko <pal16@tut.by>
2016-07-11 15:28:30 +0300

    first commit

---
+++ Hometask/src/Homework3/Task7.java
@@ -0,0 +1,20 @@
+package Homework3;
+
+public class Task7 {
+
+	public static void main(String[] args) {
+		int[] array = { 22, -3, 12, 0, -32, 34, 65, 23, 34, -65 };
+		int summa = 0;
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " | ");
+		}
+		for (int i = 0; i < array.length; i++) {
+			summa += array[i];
+			if (array[i] == 0) {
+				break;
+			}
+		}
+		System.out.println("\nСумма элементов массива до первого нуля: " + summa);
+	}
+
+}
