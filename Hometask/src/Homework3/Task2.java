cb1a84ec811f9b3f1e1cd4781a3897ea66f7f7ba
Alex Polegoshko <pal16@tut.by>
2016-07-13 16:46:35 +0300

    add comments

--- Hometask/src/Homework3/Task2.java
+++ Hometask/src/Homework3/Task2.java
@@ -3,9 +3,12 @@ package Homework3;
 public class Task2 {
 
 	public static void main(String[] args) {
-		int[] array = { 421, 42, 323, 9994, 435, 576, 347, 548, 659, 240 };
-		int min = array[0];
-		int max = array[0];
+		int[] array = { 421, 42, 323, 9994, 435, 576, 347, 548, 659, 240 }; // Initialization of the array
+		int min = array[0]; // Assigning to the variable value of the first element of the array
+		int max = array[0]; // Assigning to the variable value of the first element of the array
+		
+		/* Cycle of the searching of the min and max value of the array */
+		
 		for (int val : array) {
 			if (val < min) {
 				min = val;
@@ -14,7 +17,7 @@ public class Task2 {
 				max = val;
 			}
 		}
-		System.out.printf("min value = %,5d", min);
-		System.out.printf("\nmax value = %,5d", max);
+		System.out.printf("min value = %,5d", min); // Display of the min value of the array
+		System.out.printf("\nmax value = %,5d", max); // Display of the max value of the array
 	}
 }
