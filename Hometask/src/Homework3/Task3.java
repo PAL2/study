dccf94e423387576c7b96dffdf7b7481187b47fa
Alex Polegoshko <pal16@tut.by>
2016-07-13 16:48:52 +0300

    add comments

--- Hometask/src/Homework3/Task3.java
+++ Hometask/src/Homework3/Task3.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class Task3 {
 
 	public static void main(String[] args) {
+		
+		/* Filling in of the array with a keyboard*/
+		
 		Scanner in = new Scanner(System.in);
 		int[] array2 = new int[15];
 		for (int i = 0; i < 15; i++) {
@@ -15,6 +18,9 @@ public class Task3 {
 		for (int i = 0; i < 15; i++) {
 			System.out.printf("%3d |", array2[i]);
 		}
+		
+		/* Filling in of the array with initialization list */
+		
 		int[] array1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
 		System.out.println("\nМассив заполнен с помощью списка инициализации:");
 		for (int i = 0; i < 15; i++) {
@@ -23,12 +29,13 @@ public class Task3 {
 		int[] array3 = new int[15];
 		for (int i = 0; i < 15; i++) {
 			array3[i] = (int) (Math.random() * 100 + 1);
+			
+			/*Filling in of the array with random numbers*/
+			
 		}
-		System.out.println("\nМассив заполнен случайным образом:");
+		System.out.println("\nМассив заполнен случайными числами:");
 		for (int i = 0; i < 15; i++) {
 			System.out.printf("%3d |", array3[i]);
 		}
 	}
 }
-// System.out.print(array1[i] + " | ");
-// System.out.print(array3[i] + " | ");
